228683d330a3c98fd501272ec1a1c90b8e5f8dc1
Gabriel <gabrielestruquefassoni@gmail.com>
2024-02-20 17:07:12 -0300

    first commit

---
+++ calculadora.py
@@ -0,0 +1,18 @@
+class calculadora():
+    def adicao(num1 + num2):
+    resultado = num1 + num2
+    print (resultado)
+
+    def subtracao (num1 - num2):
+   resultado = num1 - num2
+    print (resultado)
+
+
+    def divisao (num1 / num2):
+    resultado = num1 / num2
+    print (resultado)
+
+
+    def multiplicacao (num1 * num2):
+   resultado = num1 * num2
+    print (resultado)
